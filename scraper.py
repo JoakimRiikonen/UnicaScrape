478d1e54e082e268c5532e2fe89e62e9ff269064
unknown <joakim.riiikonen@gmail.com>
2018-12-10 23:27:24 +0200

    initial commit

---
+++ scraper.py
@@ -0,0 +1,69 @@
+import requests
+import time
+import datetime
+from bs4 import BeautifulSoup
+import lxml
+import json
+
+# links to the restaurants
+links = ['assarin-ullakko',
+         'brygge',
+         'delica',
+         'deli-pharma',
+         'dental',
+         'galilei',
+         'macciavelli',
+         'nutritio',
+         'ruokakello',
+         'tottisalmi']
+
+# the data
+data = {}
+
+# the week
+date = datetime.date.today()
+# isocalendar returns tuple, week is the second value in that tuple
+data["week"] = date.isocalendar()[1]
+
+# get the menus in the restaurants
+for link in links:
+    data[link] = {}
+
+    full_addr = "https://www.unica.fi/fi/ravintolat/" + link + "/"
+    print(full_addr)
+
+    site = requests.get(full_addr)
+    if site.status_code != 200:
+        # print("Error fetching site, status code " + site.status_code)
+        # data[link]["error"] = True
+        continue
+
+    # data[link]["error"] = False
+
+    # using lxml because the html of the site is broken and html.parser doesnt work
+    soup = BeautifulSoup(site.text, 'lxml')
+
+    menuList = soup.find('div', {'class': 'menu-list'})
+    accords = menuList.find_all('div', {'class': 'accord'})
+
+    for accord in accords:
+        day = accord.find('h4').text
+        data[link][day] = {}
+        # print(day)
+        lunches = accord.find_all('td', {'class': 'lunch'})
+        prices = accord.find_all('td', {'class': 'price quiet'})
+        for i in range(len(lunches)):
+            # food item
+            lunch = lunches[i].text
+            data[link][day]["item"] = lunch
+            # price
+            price = ' '.join(prices[i].text.split())
+            data[link][day]["price"] = price
+            # print(lunches[i].text + " " + price)
+
+    time.sleep(1)
+
+    # break
+
+with open('scrapedata.json', 'w') as fp:
+    json.dump(data, fp)
